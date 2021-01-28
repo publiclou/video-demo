2edb3490f9a7b095f8113562bb194584b25b93cd
Lou Kao <lou.kao@solartninc.com>
2021-01-28 17:26:57 +0800

    collection basic style

--- src/Pages/Collection/Collection.tsx
+++ src/Pages/Collection/Collection.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react'
+import VideoList from '../../Components/VideoList/VideoList'
+import style from './Collection.module.scss'
 
 const Collection: FC = () => {
   return (
-    <div>Collection</div>
+    <div className={style['collection']}>
+      <VideoList />
+    </div>
   )
 }
 
